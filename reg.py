f780e112ad25ffd62b5f3d6e1ac9dfc2cc23c017
TAKAYUKI GOTO <114086309+gotsand@users.noreply.github.com>
2022-09-30 20:07:41 +0900

    Update reg.py

--- reg.py
+++ reg.py
@@ -61,5 +61,7 @@ df_org = pd.read_csv('master.csv', encoding = 'UTF-8-sig')
 
 df_add = pd.concat([df_org, df_d], axis=0)
 
+st.write(df_add)
+
 if st.button('データベース更新'):
     df_add.to_csv('master.csv', encoding = 'UTF-8-sig',index=False)
