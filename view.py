5eea7c6b44b05c61ae6e77547b7325f0187ae405
TAKAYUKI GOTO <114086309+gotsand@users.noreply.github.com>
2023-06-14 06:58:58 +0900

    Update view.py

--- view.py
+++ view.py
@@ -110,5 +110,5 @@ hitori = st.selectbox(label="＜ひとりごと＞", options=l_kiku)
 if hitori == '聞く':
     st.write('奥が深い')
     image = Image.open('IMG_0633.jpg')
-    im_rotate = image.rotate(90)
+    im_rotate = image.rotate(-90)
     st.image(im_rotate, caption='サンプル',use_column_width=False)
