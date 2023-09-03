3f8681665cd19dcb631e57c3a3c1db223f809c09
TAKAYUKI GOTO <114086309+gotsand@users.noreply.github.com>
2023-09-03 23:14:11 +0900

    Update view.py

--- view.py
+++ view.py
@@ -30,7 +30,7 @@ for date in l_date:
     mm = date[0:x+1]
     l_mm.append(mm)
 
-l_vmm = ['8月', '7月', '6月', '5月', '4月', '3月', '2月', '1月'] 
+l_vmm = ['9月', '8月', '7月', '6月', '5月', '4月', '3月', '2月', '1月'] 
 #l_vmm = ['12月', '11月', '10月', '9月']
 
 target = st.selectbox(label="＜表示月選択＞", options=l_vmm)
@@ -108,4 +108,4 @@ st.bar_chart(df_p)
 l_kiku = ['聞かない', '聞く']
 hitori = st.selectbox(label="＜ひとりごと＞", options=l_kiku)
 if hitori == '聞く':
-    st.write('じいさんの実家があったとのことで、仕事帰りに江東区の深川を古地図散歩してみた。昔も都会のよう。')
+    st.write('バスケットおめでとう。ＦＣ岐阜は強いの？')
