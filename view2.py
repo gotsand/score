e6ed83bea89963881a76587f0ede5ca3662e7990
TAKAYUKI GOTO <114086309+gotsand@users.noreply.github.com>
2024-07-09 08:17:10 +0900

    Update view2.py

--- view2.py
+++ view2.py
@@ -30,7 +30,7 @@ for date in l_date:
     mm = date[0:x+1]
     l_mm.append(mm)
 
-l_vmm = ['5月', '4月', '3月', '2月', '1月', '12月', '11月'] 
+l_vmm = ['7月', '6月', '5月', '4月', '3月', '2月', '1月'] 
 #l_vmm = ['12月', '11月', '10月', '9月']
 
 target = st.selectbox(label="＜表示月選択＞", options=l_vmm)
@@ -108,6 +108,7 @@ st.bar_chart(df_p)
 l_kiku = ['聞かない', '聞く']
 hitori = st.selectbox(label="＜ひとりごと＞", options=l_kiku)
 if hitori == '聞く':
-    st.write('単身赴任２年目突入、名古屋に転勤の気配はありませんね')
-    image = Image.open('IMG_2185.jpg')
-    st.image(image, caption='ヤクルトファン化',use_column_width=True)
+    st.write('６月は働きすぎ飲みすぎ、７月は充電期間')
+#    image = Image.open('IMG_2185.jpg')
+#
+st.image(image, caption='ヤクルトファン化',use_column_width=True)
